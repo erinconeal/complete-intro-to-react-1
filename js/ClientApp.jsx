a83f4b2cb7123de6daf13b3afc23f1736c4f59a1
eco <erinconeal@outlook.com>
2017-10-01 16:53:17 -0400

    set up React helper files

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 const MyTitle = function(props) {
   return ce(
-    "div",
+    'div',
     null,
-    ce("h1", { style: { color: props.color } }, props.title)
+    ce('h1', { style: { color: props.color } }, props.title)
   );
 };
 
 const MyFirstComponent = function() {
   return ce(
-    "div",
-    { id: "my-first-component" },
-    ce(MyTitle, { title: "Haven", color: "YellowGreen" }),
-    ce(MyTitle, { title: "Doc Martin", color: "GreenYellow" }),
-    ce(MyTitle, { title: "The Followng", color: "LimeGreen" }),
-    ce(MyTitle, { title: "Stranger Things", color: "Peru" })
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { title: 'Haven', color: 'YellowGreen' }),
+    ce(MyTitle, { title: 'Doc Martin', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'The Following', color: 'LimeGreen' }),
+    ce(MyTitle, { title: 'Stranger Things', color: 'Peru' })
   );
 };
 
-ReactDOM.render(
-  React.createElement(MyFirstComponent),
-  document.getElementById("app")
-);
+render(ce(MyFirstComponent), document.getElementById('app'));
